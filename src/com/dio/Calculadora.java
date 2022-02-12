e5bb797c8fbf5c29c03cb952fa1811373a63d15b
Thales Galvão <tera.thalespolo@gmail.com>
2022-02-12 17:55:33 -0300

    update import

--- src/com/dio/Calculadora.java
+++ src/com/dio/Calculadora.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Calculadora {
 
-	//Calculator test
+	//Calculator test with imoprt
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
